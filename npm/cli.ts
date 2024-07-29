96624809cf9f192b19329242d4d4d4c2aaaca906
Artem Chystiakov <artem.ch31@gmail.com>
2024-07-29 16:57:29 +0300

    fix prettier

--- npm/cli.ts
+++ npm/cli.ts
@@ -1,39 +1,39 @@
 #!/usr/bin/env node
 
-import { CircomRunner } from './index';
-import fs from 'fs';
-import path from 'path';
+import { CircomRunner } from "./index";
+import fs from "fs";
+import path from "path";
 
 async function main() {
-    let args = process.argv
-        .slice(2)
-        .map((k) => (k.startsWith('-') ? k : k.endsWith('/') ? path.resolve(k) + '/' : path.resolve(k)));
+  let args = process.argv
+    .slice(2)
+    .map((k) => (k.startsWith("-") ? k : k.endsWith("/") ? path.resolve(k) + "/" : path.resolve(k)));
 
-    if (!(args.includes('-o') || args.includes('--output'))) {
-        args.push('-o', process.cwd() + '/');
-    }
+  if (!(args.includes("-o") || args.includes("--output"))) {
+    args.push("-o", process.cwd() + "/");
+  }
 
-    if (args.length === 0) {
-        args.push('--help');
-    }
+  if (args.length === 0) {
+    args.push("--help");
+  }
 
-    const circom = new CircomRunner({
-        args,
-        env: process.env,
-        preopens: {"/":"/"},
-    });
+  const circom = new CircomRunner({
+    args,
+    env: process.env,
+    preopens: { "/": "/" },
+  });
 
-    const wasm_bytes = fs.readFileSync(require.resolve('./circom.wasm'));
+  const wasm_bytes = fs.readFileSync(require.resolve("./circom.wasm"));
 
-    // There is a slight delay between this logging and the circom compiler version logging
-    if (args.includes('--version')) {
-        console.log('circom2 npm package', require('./package.json').version);
-    }
+  // There is a slight delay between this logging and the circom compiler version logging
+  if (args.includes("--version")) {
+    console.log("circom2 npm package", require("./package.json").version);
+  }
 
-    await circom.execute(wasm_bytes);
+  await circom.execute(wasm_bytes);
 }
 
 main().catch((err) => {
-    console.error(err);
-    process.exit(1);
-})
+  console.error(err);
+  process.exit(1);
+});
